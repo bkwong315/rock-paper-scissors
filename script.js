629b16209a6203f581f19771c438e210582dc6a6
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2022-12-19 18:13:30 -0800

    Add logic to compute one round

--- script.js
+++ script.js
@@ -2,3 +2,53 @@ const getComputerChoice = () => {
   let choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * 3)];
 };
+
+const computeRound = (playerSelection, computerSelection) => {
+  let playerSelectionLower = playerSelection.toLowerCase();
+  let computerSelectionLower = computerSelection.toLowerCase();
+  let result = "";
+
+  switch (playerSelectionLower) {
+    case "rock":
+      switch (computerSelectionLower) {
+        case "paper":
+          result = "You Lose!";
+          break;
+        case "rock":
+          result = "That's a Draw!";
+          break;
+        case "scissors":
+          result = "You Win!";
+          break;
+      }
+      break;
+    case "paper":
+      switch (computerSelectionLower) {
+        case "scissors":
+          result = "You Lose!";
+          break;
+        case "paper":
+          result = "That's a Draw!";
+          break;
+        case "rock":
+          result = "You Win!";
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerSelectionLower) {
+        case "rock":
+          result = "You Lose!";
+          break;
+        case "scissors":
+          result = "That's a Draw!";
+          break;
+        case "paper":
+          result = "You Win!";
+          break;
+      }
+      break;
+  }
+
+  return result;
+};
